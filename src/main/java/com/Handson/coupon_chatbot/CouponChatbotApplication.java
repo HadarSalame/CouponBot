94d33585da575c582433073ad600fe21d5d5dfe6
HadarSalame <hadarSalame99@gmail.com>
2023-04-23 22:15:48 +0300

    with bot

---
+++ src/main/java/com/Handson/coupon_chatbot/CouponChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.Handson.coupon_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Handson/coupon_chatbot/CouponChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Handson.coupon_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
